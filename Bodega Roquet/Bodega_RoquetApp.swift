438cc7ac514a7ff55e0703a64b60b605698d3fa4
Daniel Araujo <danielaraujonobre14@gmail.com>
2023-04-30 19:04:52 -0300

    Initial Commit

---
+++ Bodega Roquet/Bodega_RoquetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bodega_RoquetApp.swift
+//  Bodega Roquet
+//
+//  Created by Daniel Nobre on 30/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Bodega_RoquetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bodega Roquet/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Bodega Roquet
+//
+//  Created by Daniel Nobre on 30/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
